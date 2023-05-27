67ded88a23fcdd1d2ef68d9eb5d4df0d2b00749e
g-vega-cl <g.vega.cl@gmail.com>
2023-05-27 10:14:22 -0400

    get prices for a strike price per week

--- get-historical-prices.py
+++ get-historical-prices.py
@@ -1,23 +1,44 @@
 from ib_insync import *
-# util.startLoop()  # uncomment this line when in a notebook
+import pandas as pd
+import datetime
 
 ib = IB()
 ib.connect('127.0.0.1', 7496, clientId=1)
 
-year = "2023"
-month = "6".zfill(2)
-day = "2".zfill(2)
+# Specify the initial date
+year = 2023
+month = 6
+day = 2
+start_date = datetime.date(year, month, day)
 
-date = year + month + day
+# Initialize an empty list to store week/price pairs
+weekly_prices = []
+week_index = 1
+# Retrieve prices for 7 days from the initial date
+for i in range(7):
+    date = start_date + datetime.timedelta(days=i*7)
 
-# TICKER - year, month, day - strike - P or C - exchange
-contract = Option('TQQQ', date, 30, 'P', 'SMART')
+    # TICKER - year, month, day - strike - P or C - exchange
+    contract = Option('TQQQ', date.strftime('%Y%m%d'), 30, 'P', 'SMART')
 
-# Fetching historical bid prices
-bars = ib.reqHistoricalData(
-    contract, endDateTime='', durationStr='1 D',
-    barSizeSetting='1 hour', whatToShow='BID', useRTH=True)
+    # Fetch historical bid prices
+    bars = ib.reqHistoricalData(
+        contract, endDateTime='', durationStr='1 D',
+        barSizeSetting='1 hour', whatToShow='BID', useRTH=True)
 
-# convert to pandas dataframe:
-df = util.df(bars)
-print(df)
+    # Check if bars is None or empty
+    if bars is not None and len(bars):
+        # Convert to pandas dataframe:
+        df = util.df(bars)
+        
+        # Get the last price for the day
+        last_price = df['close'].iloc[-1]
+
+        # Append week/price pair to the list
+        weekly_prices.append({'Week': week_index, 'Price': last_price})
+        week_index += 1
+# Create a DataFrame from the list
+df_weekly_prices = pd.DataFrame(weekly_prices)
+
+# Print the dataframe
+print(df_weekly_prices)
