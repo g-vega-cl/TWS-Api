85ddb7cc3614d262db45e5eb11b9fd09f1d5cbce
g-vega-cl <g.vega.cl@gmail.com>
2023-06-04 20:21:18 -0400

    add max_weekly_gains

--- get-historical-prices.py
+++ get-historical-prices.py
@@ -2,9 +2,27 @@ from ib_insync import *
 import pandas as pd
 import datetime
 import sys
+import math
+
+
+maintainanance_margin_dict = {
+    "TQQQ": 1700,
+    "PLTR": 300,
+    "SOXL": 1400,
+    "EEM": 880
+}
 
 ib = IB()
 ib.connect('127.0.0.1', 7496, clientId=1)
+
+# Get available liquidity
+account_values = ib.accountValues()
+excess_liquidity = 0
+
+for i in range(len(account_values)):
+    if account_values[i].tag == 'ExcessLiquidity':
+        excess_liquidity = float(account_values[i].value)
+print(f'Available liquidity: {excess_liquidity}')
 
 # Specify the initial date
 # Get today's date
@@ -16,14 +34,21 @@ while start_date.weekday() != 4:  # 4 corresponds to Friday
 
 # Initialize an empty list to store week/price pairs
 weekly_prices = []
+max_weekly_gains = []
 week_index = 0
 # Retrieve prices for 7 days from the initial date
-strike = 36
-for i in range(9):
-    date = start_date + datetime.timedelta(days=i*7)
+strike = 37
+comission = 0.015
+
+ticker = 'TQQQ'
+for week_number in range(9):
+    date = start_date + datetime.timedelta(days=week_number*7)
     week_index += 1
     # TICKER - year, month, day - strike - P or C - exchange
-    contract = Option('TQQQ', date.strftime('%Y%m%d'), strike, 'P', 'SMART')
+    contract = Option(ticker, date.strftime('%Y%m%d'), strike, 'P', 'SMART')
+
+    maintainanance_margin = maintainanance_margin_dict[ticker]
+    max_orders = math.floor(excess_liquidity/maintainanance_margin)
 
     # Fetch historical bid prices
     bars = ib.reqHistoricalData(
@@ -38,12 +63,16 @@ for i in range(9):
         # Get the last price for the day
         last_price = df['close'].iloc[-1]
 
+        last_max_weekly_gain = max_orders * (last_price - comission) / week_index * 100
         # Append week/price pair to the list
         weekly_prices.append({'Week': week_index, 'Price': last_price, "Price / Week": round(last_price / week_index,3)})
-        
+        max_weekly_gains.append({'Week': week_index, 'Price': last_price, "Price / Week": round(last_price / week_index,3), "Max Weekly Gain": round(last_max_weekly_gain,3)})
+
 # Create a DataFrame from the list
 df_weekly_prices = pd.DataFrame(weekly_prices)
+df_max_weekly_gains = pd.DataFrame(max_weekly_gains)
 
 # Print the dataframe
+print(df_max_weekly_gains)
 print(df_weekly_prices)
 df_weekly_prices.to_csv(sys.stdout, index=False)
