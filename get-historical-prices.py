8235395cfbd45a101c90dd62243bf38373c5e480
g-vega-cl <g.vega.cl@gmail.com>
2023-11-05 19:00:48 -0500

    with short

--- get-historical-prices.py
+++ get-historical-prices.py
@@ -4,41 +4,73 @@ import datetime
 import sys
 import math
 
-maintainanance_margin_dict = {
-    "TQQQ": 1700,
-    "PLTR": 300,
-    "SOXL": 1400,
-    "EEM": 880,
-    "IWM": 3700,
-    "ARKK": 800,
-    "LNG": 3800,
+# NEED TO CONFIGURE WITH UBUNTU, BUT YOU CAN RUN WITH WINDOWS BASH
+
+# stocks
+stocks = {
+    "TQQQ": {
+        "current_price": 42,
+        "maintainanance_margin": 1700,
+        "short": False,
+    },
+    "SQQQ": {
+        "current_price": 20,
+        "maintainanance_margin": 1250,
+        "short": True,
+    },
+    "SPXL": {
+        "current_price": 80,
+        "maintainanance_margin": 5000,
+        "short": False,
+    },
+    "SPXU": {
+        "current_price": 12,
+        "maintainanance_margin": 750,
+        "short": True,
+    },
+    "SDOW": {
+        "current_price": 27,
+        "maintainanance_margin": 1700,
+        "short": True,
+    },
+    "SRTY": {
+        "current_price": 52,
+        "maintainanance_margin": 3000,
+        "short": True,
+    },
+    "LNG": {
+        "current_price": 167,
+        "maintainanance_margin": 3800,
+        "short": False,
+    }
 }
 
-# rounded up to the nearest dollar, unless really close to current value
-current_prices = {
-    "TQQQ": 42,
-    "PLTR": 18,
-    "SOXL": 25,
-    "EEM": 41,
-    "IWM": 195,
-    "ARKK": 45,
-    "LNG": 167,
-}
+# Get current prices out of the stocks object
+current_prices = {}
+for key in stocks.keys():
+    current_prices[key] = stocks[key]["current_price"]
 
-def calculate_strike_percent(percent, current_prices):
+
+
+
+def calculate_strike_percent(percent, stocks):
     strike_percent = {}
-    for key, value in current_prices.items():
+    for key in stocks.keys():
+        value = stocks[key]["current_price"]
         strike_percent[key] = math.floor(value * percent)
     return strike_percent
 
-strike_3_percent = calculate_strike_percent(0.97, current_prices)
-print(strike_3_percent)
+strike_3_percent = calculate_strike_percent(0.97, stocks)
 
-strike_5_percent = calculate_strike_percent(.95, current_prices)
-print(strike_5_percent)
+strike_5_percent = calculate_strike_percent(.95, stocks)
 
-strike_9_percent = calculate_strike_percent(.91, current_prices)
-print(strike_9_percent)
+strike_9_percent = calculate_strike_percent(.91, stocks)
+
+strike_neg_9_percent = calculate_strike_percent(1.09, stocks)
+
+strike_neg_5_percent = calculate_strike_percent(1.05, stocks)
+
+strike_neg_3_percent = calculate_strike_percent(1.03, stocks)
 
 
 ib = IB()
@@ -63,31 +95,57 @@ comission = 0.015
 max_weekly_gains = []
 
 
-for ticker in maintainanance_margin_dict.keys():
-    for strike_percent in [current_prices, strike_3_percent, strike_5_percent, strike_9_percent]:
-        weekly_prices = []
-        week_index = 0
-        strike = strike_percent[ticker]
-        for week_number in range(9):
-            date = start_date + datetime.timedelta(days=week_number*7)
-            week_index += 1
-            contract = Option(ticker, date.strftime('%Y%m%d'), strike, 'P', 'SMART')
+for ticker in stocks.keys():
+    if(stocks[ticker]["short"]):
+        for strike_percent in [current_prices, strike_neg_3_percent, strike_neg_5_percent, strike_neg_9_percent]:
+            weekly_prices = []
+            week_index = 0
+            strike = strike_percent[ticker]
+            for week_number in range(9):
+                date = start_date + datetime.timedelta(days=week_number*7)
+                week_index += 1
+                contract = Option(ticker, date.strftime('%Y%m%d'), strike, 'C', 'SMART')
 
-            maintainanance_margin = maintainanance_margin_dict[ticker]
-            max_orders = math.floor(excess_liquidity/maintainanance_margin)
+                maintainanance_margin = stocks[ticker]["maintainanance_margin"]
+                max_orders = math.floor(excess_liquidity/maintainanance_margin)
 
-            bars = ib.reqHistoricalData(
-                contract, endDateTime='', durationStr='1 D',
-                barSizeSetting='1 hour', whatToShow='BID', useRTH=True)
+                bars = ib.reqHistoricalData(
+                    contract, endDateTime='', durationStr='1 D',
+                    barSizeSetting='1 hour', whatToShow='BID', useRTH=True)
 
-            if bars is not None and len(bars):
-                df = util.df(bars)
-                last_price = df['close'].iloc[-1]
-                last_max_weekly_gain = max_orders * (last_price - comission) / week_index * 100
-                weekly_prices.append({'Week': week_index, 'Price': last_price})
-                max_weekly_gains.append({'Ticker': ticker, 'Week': week_index, 'Price': last_price, "Price / Week": round(last_price / week_index,3), "strike":strike, "Max Weekly Gain": round(last_max_weekly_gain,3), "Maintainace margin": maintainanance_margin_dict[ticker]})
+                if bars is not None and len(bars):
+                    df = util.df(bars)
+                    last_price = df['close'].iloc[-1]
+                    last_max_weekly_gain = max_orders * (last_price - comission) / week_index * 100
+                    weekly_prices.append({'Week': week_index, 'Price': last_price})
+                    max_weekly_gains.append({'Ticker': ticker, 'Week': week_index, 'Price': last_price, "Price / Week": round(last_price / week_index,3), "current_price":strike, "Max Weekly Gain": round(last_max_weekly_gain,3), "Maintainace margin": maintainanance_margin})
 
-        df_weekly_prices = pd.DataFrame(weekly_prices)
+            # df_weekly_prices = pd.DataFrame(weekly_prices)
+    else:
+        for strike_percent in [current_prices, strike_3_percent, strike_5_percent, strike_9_percent]:
+            weekly_prices = []
+            week_index = 0
+            strike = strike_percent[ticker]
+            for week_number in range(9):
+                date = start_date + datetime.timedelta(days=week_number*7)
+                week_index += 1
+                contract = Option(ticker, date.strftime('%Y%m%d'), strike, 'P', 'SMART')
+
+                maintainanance_margin = stocks[ticker]["maintainanance_margin"]
+                max_orders = math.floor(excess_liquidity/maintainanance_margin)
+
+                bars = ib.reqHistoricalData(
+                    contract, endDateTime='', durationStr='1 D',
+                    barSizeSetting='1 hour', whatToShow='BID', useRTH=True)
+
+                if bars is not None and len(bars):
+                    df = util.df(bars)
+                    last_price = df['close'].iloc[-1]
+                    last_max_weekly_gain = max_orders * (last_price - comission) / week_index * 100
+                    weekly_prices.append({'Week': week_index, 'Price': last_price})
+                    max_weekly_gains.append({'Ticker': ticker, 'Week': week_index, 'Price': last_price, "Price / Week": round(last_price / week_index,3), "current_price":strike, "Max Weekly Gain": round(last_max_weekly_gain,3), "Maintainace margin": maintainanance_margin})
+
+            # df_weekly_prices = pd.DataFrame(weekly_prices)
 
 df_max_weekly_gains = pd.DataFrame(max_weekly_gains)
 print(df_max_weekly_gains)
